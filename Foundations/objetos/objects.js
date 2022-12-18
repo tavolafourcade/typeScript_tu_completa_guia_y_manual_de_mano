65e42a678c9dac35cd0bf589cdbd329b172a7b19
tavolafourcade <tavolafourcade@gmail.com>
2022-12-18 18:26:41 -0500

    [ ADD ] Métodos dentro de los objetos

--- Foundations/objetos/objects.js
+++ Foundations/objetos/objects.js
@@ -15,4 +15,13 @@
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     };
+    flash = {
+        name: 'Clark Kent',
+        // age: 24,
+        powers: ['Super fuerza'],
+        getName() {
+            return this.name;
+        }
+    };
+    console.log(flash.getName());
 })();
--- Foundations/objetos/objects.ts
+++ Foundations/objetos/objects.ts
@@ -9,9 +9,20 @@
       }
   */
   () => {
-  let flash: {name: string, age: number, powers: string[]} = {
+  let flash: {name: string, age?: number, powers: string[], getName?: () => string} = {
     name: 'Barry Allen',
     age: 24,
     powers: ['Super velocidad', 'Viajar en el tiempo']
   }
+
+  flash = {
+    name: 'Clark Kent',
+    // age: 24,
+    powers: ['Super fuerza'],
+    getName(){
+      return this.name
+    }
+  }
+
+  console.log(flash.getName!())
 })()
