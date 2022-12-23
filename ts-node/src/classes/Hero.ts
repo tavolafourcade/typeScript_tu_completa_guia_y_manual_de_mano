986dc2af83e8b43b9c0602dd1aef2ea864e3e2e4
tavolafourcade <tavolafourcade@gmail.com>
2022-12-23 18:20:14 -0500

    [ ADD ] Tarea - resolver errores en TypeScript: use of ? for avoid the undefined error

--- ts-node/src/classes/Hero.ts
+++ ts-node/src/classes/Hero.ts
@@ -1,9 +1,15 @@
+import powers from "../data/powers";
+
 export class Hero {
   constructor (
     public name: string,
     public powerId: number,
     public age: number
     ) {}
+
+    get power(): string {
+      return powers.find(power => power.id === this.powerId)?.desc || 'not found'
+    }
 }
 
 export class Hero2 {}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,16 +1,18 @@
 // import { Hero as SuperHero, Hero2 } from './classes/Hero'; // Le damos el alias SuperHero a Hero
-import * as HeroClasses from './classes/Hero' // Alias completo a todo lo que exporta el módulo
-import powers, {Power} from './data/powers';
-
-const Hero = 123
+// import * as HeroClasses from './classes/Hero' // Alias completo a todo lo que exporta el módulo
+// import powers, {Power} from './data/powers';
+import { Hero } from './classes/Hero'
+// const Hero = 123
 
 // const ironman = new SuperHero('Ironman', 1, 55)
 
-const ironman = new HeroClasses.Hero('Ironman', 1, 55)
+// const ironman = new HeroClasses.Hero('Ironman', 1, 55)
+const ironman = new Hero('Ironman', 1, 55)
 
 console.log(ironman);
+console.log(ironman.power);
 
-console.log({powers})
+
 
 
 
