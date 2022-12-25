f9916783e597977e5315038b518c80c3d75bdf4f
tavolafourcade <tavolafourcade@gmail.com>
2022-12-24 20:38:46 -0500

    [ ADD ] Intro a los genéricos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,16 +1,26 @@
 // import { Hero as SuperHero, Hero2 } from './classes/Hero'; // Le damos el alias SuperHero a Hero
 // import * as HeroClasses from './classes/Hero' // Alias completo a todo lo que exporta el módulo
 // import powers, {Power} from './data/powers';
-import { Hero } from './classes/Hero'
+// import { Hero } from './classes/Hero'
+import { printObject, genericFunction } from './generics/generics';
 // const Hero = 123
 
 // const ironman = new SuperHero('Ironman', 1, 55)
 
 // const ironman = new HeroClasses.Hero('Ironman', 1, 55)
-const ironman = new Hero('Ironman', 1, 55)
+// const ironman = new Hero('Ironman', 1, 55)
 
-console.log(ironman);
-console.log(ironman.power);
+// console.log(ironman);
+// console.log(ironman.power);
+
+////////////////////////////////////////////////
+
+printObject( 123 )
+printObject( new Date() )
+printObject( 'Hola Mundo' )
+
+console.log(genericFunction(3.14).toFixed(2))
+console.log(genericFunction('Hola').toFixed(2))
 
 
 
