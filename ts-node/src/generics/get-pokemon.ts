ce9f1ad7b17ee2797e3fb5c15a7eb21ea50508f3
tavolafourcade <tavolafourcade@gmail.com>
2022-12-25 19:31:46 -0500

    [ ADD ] Mapear respuestas http y uso de quicktype para obtener el tipado de JSON

--- ts-node/src/generics/get-pokemon.ts
+++ ts-node/src/generics/get-pokemon.ts
@@ -1,9 +1,17 @@
 import axios from 'axios'
+import { Pokemon } from '../interfaces/pokemon';
 
 // Estamos creando una función que retorna una promesa con tipo genérico number
-export const getPokemon = async (pokemonId: number) => {
-  const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-  console.log({resp})
-  return 1
+// Se creó una interfaz para Pokemon que asegura que lo que venga se va a usar bajo dicha estructura.
+//*  OJO: con el uso de <Pokemon> no estamos transformando la respuesta, lo que pasa es que ahora TS va a considerar que la petición GET será de tipo Pokemon.
+//*       y si le ponemos el punto después de resp.data. nos dará las opciones de la Interfaz Pokemon
+// export const getPokemon = async (pokemonId: number): Promise<number> => {
+//   const { data } = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+//   console.log(data.abilities[0].ability.name)
+//   return 1
+// }
+//* En este caso especificamos que retornará una Promesa de tipo Pokemon
+export const getPokemon = async (pokemonId: number): Promise<Pokemon> => {
+  const { data } = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+  return data
 }
-
